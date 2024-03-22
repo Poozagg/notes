5516ef3ffa1e7e020c9818651193f9900912baa2
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-22 12:16:43 +0000

    createdAt & updatedAt properties added to the note object - in App componenet

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,10 @@ function App() {
   const [notes, setNotes] = useState([])
   const [currentNoteId, setCurrentNoteId] = useState("")
 
+  const currentNote =
+  notes.find(note => note.id === currentNoteId)
+  || notes[0]
+
   // we want this useEffect to run every time the notes array changes
   useEffect(() => {
     const unsubscribe = onSnapshot(notesCollection, function(snapshot) {
@@ -36,7 +40,9 @@ function App() {
 
   async function createNewNote() {
       const newNote = {
-          body: "# Type your markdown note's title here"
+          body: "# Type your markdown note's title here",
+          createdAt: Date.now(),
+          updatedAt: Date.now()
       }
       const newNoteRef = await addDoc(notesCollection, newNote)
       setCurrentNoteId(newNoteRef.id)
@@ -45,7 +51,7 @@ function App() {
 
   async function updateNote(text) {
     const docRef = doc(db, "notes", currentNoteId)
-    await setDoc(docRef, {body: text}, {merge: true})
+    await setDoc(docRef, {body: text, updatedAt: Date.now()}, {merge: true})
   }
 
   async function deleteNote(noteId) {
